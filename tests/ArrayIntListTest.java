05146a2d6c542b27edf26890cd0d9e5b5a421699
theZalmanian <thezalmanian@gmail.com>
2024-01-22 15:18:10 -0800

    Created tests for all ArrayIntList methods

---
+++ tests/ArrayIntListTest.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ArrayIntListTest {
+    /**
+     * Static ArrayIntList so there is one version to test with
+     */
+    private static ArrayIntList testArrayIntList;
+
+    /**
+     * Set up the test ArrayIntList with basic values before tests are run
+     */
+    @BeforeAll
+    static void setupArrayIntList() {
+        testArrayIntList = new ArrayIntList();
+    }
+
+    @Test
+    void addFront() {
+    }
+
+    @Test
+    void addBack() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void removeFront() {
+    }
+
+    @Test
+    void removeBack() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void contains() {
+
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void iterator() {
+    }
+}
