15f2fdf06a0273b6a06c5ac729ee5cbd4ded7914
theZalmanian <thezalmanian@gmail.com>
2024-01-26 02:28:13 -0800

    Implemented tests for DoublyLinkedIntList's clear, size, and isEmpty methods

--- tests/DoublyLinkedIntListTest.java
+++ tests/DoublyLinkedIntListTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class DoublyLinkedIntListTest {
     /**
      * Create LinkedIntList at start
@@ -63,14 +65,57 @@ class DoublyLinkedIntListTest {
     }
 
     @Test
-    void isEmpty() {
+    void isEmpty_listContainsMultipleNodes_returnsSize() {
+        // add values so the list is not empty
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+
+        // check if list is empty
+        assertFalse(testDoublyLinkedIntList.isEmpty());
     }
 
     @Test
-    void size() {
+    void isEmpty_listEmpty_returnsSize() {
+        // ensure list is empty
+        assertTrue(testDoublyLinkedIntList.isEmpty());
     }
 
     @Test
-    void clear() {
+    void size_listContainsMultipleNodes_returnsSize() {
+        // add values so the list is not empty
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+
+        assertEquals(3, testDoublyLinkedIntList.size());
+    }
+
+    @Test
+    void size_listEmpty_returnsSize() {
+        assertEquals(0, testDoublyLinkedIntList.size());
+    }
+
+    @Test
+    void clear_listContainsMultipleNodes_clearsSuccessfully() {
+        // add values so list is not empty
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+        testDoublyLinkedIntList.addBack(FILLER_VALUE);
+
+        // clear the list
+        testDoublyLinkedIntList.clear();
+
+        // ensure list is empty
+        assertTrue(testDoublyLinkedIntList.isEmpty());
+    }
+
+    @Test
+    void clear_listEmpty_clearsSuccessfully() {
+        // clear the list
+        testDoublyLinkedIntList.clear();
+
+        // ensure list is empty
+        assertTrue(testDoublyLinkedIntList.isEmpty());
     }
 }
